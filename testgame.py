cd76cf57b4445570a0fe4af8c371517ab307eb11
adiathasan <adiathasan.me@gmail.com>
2020-06-07 22:03:00 -0700

    player can jump and move around.

--- testgame.py
+++ testgame.py
@@ -10,29 +10,46 @@ window = pygame.display.set_mode((600, 400))
 x = 50
 y = 50
 width = 40
-height = 60
-vel = 5
-
+height = 50
+vel = 10
+jump_count = 10
+jump = False
 run = True
-
+a = 0
 while run:
     pygame.time.delay(100)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            break
+            run = False
 
     keys = pygame.key.get_pressed()
+    if not jump:
+        if keys[pygame.K_UP] and y > 0:
+            y -= vel
+        if keys[pygame.K_DOWN] and y < 400 - height:
+            y += vel
+        if keys[pygame.K_SPACE]:
+            jump = True
+    else:
+        if jump_count >= -10:
+            y -= (jump_count * abs(jump_count)) * .5
+            jump_count -= 2
+            print(y)
+        else:
+            jump = False
+            jump_count = 10
+    if keys[pygame.K_LEFT] and x > 0:
+        x -= vel
+    if keys[pygame.K_RIGHT] and x < 600 - width:
+        x += vel
 
-    # if keys[pygame.K_UP]:
-    #
-    # if keys[pygame.K_DOWN]:
-    #
-    # if keys[pygame.K_LEFT]:
-    #
-    # if keys[pygame.K_RIGHT]:
-
-    pygame.draw.rect(window, (255, 9, 0), (x, y, width, height))
+    window.fill((0, 0, 0))
+    a += 11
+    try:
+        pygame.draw.rect(window, (a, 9, 0), (x, y, width, height))
+    except TypeError:
+        a = 10
     pygame.display.update()
 
 pygame.quit()
