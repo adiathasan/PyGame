5d0a28b1a7ef2ad4853368a4ae9cd8ecce3b24af
adiathasan <adiathasan.me@gmail.com>
2020-06-08 01:37:34 -0700

    set char movement and animation.

--- testgame.py
+++ testgame.py
@@ -4,20 +4,61 @@ pygame.init()
 
 pygame.display.set_caption('PUBG')
 
-window = pygame.display.set_mode((600, 400))
+window = pygame.display.set_mode((600, 470))
+walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'),
+             pygame.image.load('R3.png'), pygame.image.load('R4.png'),
+             pygame.image.load('R5.png'), pygame.image.load('R6.png'),
+             pygame.image.load('R7.png'), pygame.image.load('R8.png'),
+             pygame.image.load('R9.png')]
+
+walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'),
+            pygame.image.load('L3.png'), pygame.image.load('L4.png'),
+            pygame.image.load('L5.png'), pygame.image.load('L6.png'),
+            pygame.image.load('L7.png'), pygame.image.load('L8.png'),
+            pygame.image.load('L9.png')]
+bg = pygame.image.load('bg.jpg')
+
+timeFrame = pygame.time.Clock()
+
+char = pygame.image.load('standing.png')
 
 
-x = 50
-y = 50
-width = 40
-height = 50
+# class Player
+
+x = 0
+y = 400
+width = 64
+height = 64
 vel = 10
 jump_count = 10
 jump = False
 run = True
+left = False
+right = False
+doz = False
+walk_count = 0
 a = 0
+
+
+def charDraw():
+    global walk_count, height, do
+    window.blit(bg, (0, 0))
+    if walk_count + 1 >= 9:
+        walk_count = 0
+    if right:
+        window.blit(walkRight[walk_count], (x, y))
+        walk_count += 1
+    elif left:
+        window.blit(walkLeft[walk_count], (x, y))
+        walk_count += 1
+    else:
+        window.blit(char, (x, y))
+
+    pygame.display.update()
+
+
 while run:
-    pygame.time.delay(100)
+    timeFrame.tick(14)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -25,10 +66,6 @@ while run:
 
     keys = pygame.key.get_pressed()
     if not jump:
-        if keys[pygame.K_UP] and y > 0:
-            y -= vel
-        if keys[pygame.K_DOWN] and y < 400 - height:
-            y += vel
         if keys[pygame.K_SPACE]:
             jump = True
     else:
@@ -40,16 +77,19 @@ while run:
             jump = False
             jump_count = 10
     if keys[pygame.K_LEFT] and x > 0:
+        left = True
+        right = False
         x -= vel
-    if keys[pygame.K_RIGHT] and x < 600 - width:
+    elif keys[pygame.K_RIGHT] and x < 600 - width:
+        left = False
+        right = True
         x += vel
+    elif keys[pygame.K_DOWN]:
+        doz = True
+    else:
+        left = False
+        right = False
 
-    window.fill((0, 0, 0))
-    a += 11
-    try:
-        pygame.draw.rect(window, (a, 9, 0), (x, y, width, height))
-    except TypeError:
-        a = 10
-    pygame.display.update()
+    charDraw()
 
 pygame.quit()
